0ae6c13e40f6af797aa5957d0ebbd7deaad9327c
Audrey <lemeu001@umn.edu>
2020-09-06 18:33:40 -0500

    Complete third regex expression

--- regex/regex.sh
+++ regex/regex.sh
@@ -4,4 +4,4 @@ awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n" "
 
 awk 'match($0, /* I am ([a-zA-Z]+). My favorite sandwich is ([a-zA-Z]+)./, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' r1_input.txt > r1_output.txt
 
-awk 'match($0, /* sandwich with ([a-zA-Z.]+). ([a-zA-Z]+_ )/, groups) {print "1. " groups[1] ".\n" "2. " groups[2] "\n" }' r2_input.txt > r2_output.txt
+awk 'match($0, /* sandwich with ([a-zA-Z.]+). ([a-zA-Z ]+)/, groups) {print "1. " groups[1] ".\n" "2. " groups[2] "\n" }' r2_input.txt > r2_output.txt
